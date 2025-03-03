0be85abc81502bf6497ffc0cd13c89ed4a6c66c0
liuhaoyu <1349808913@qq.com>
2025-03-03 17:26:18 +0800

    Initial commit

---
+++ FileExtensionChanger/FileExtensionChanger.Designer.cs
@@ -0,0 +1,272 @@
+﻿using System.Windows.Forms;
+
+namespace FileExtensionChanger
+{
+    partial class FileExtensionChangerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxFolder = new System.Windows.Forms.GroupBox();
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBoxFiles = new System.Windows.Forms.GroupBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnDeselectAll = new System.Windows.Forms.Button();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.groupBoxNewExtension = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNewExtension = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBoxFolder.SuspendLayout();
+            this.groupBoxFiles.SuspendLayout();
+            this.groupBoxNewExtension.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxFolder
+            // 
+            this.groupBoxFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxFolder.Controls.Add(this.btnBrowseFolder);
+            this.groupBoxFolder.Controls.Add(this.txtFolderPath);
+            this.groupBoxFolder.Controls.Add(this.label1);
+            this.groupBoxFolder.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxFolder.Name = "groupBoxFolder";
+            this.groupBoxFolder.Size = new System.Drawing.Size(760, 60);
+            this.groupBoxFolder.TabIndex = 0;
+            this.groupBoxFolder.TabStop = false;
+            this.groupBoxFolder.Text = "目录选择";
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseFolder.Location = new System.Drawing.Point(677, 22);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFolder.TabIndex = 2;
+            this.btnBrowseFolder.Text = "浏览...";
+            this.btnBrowseFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolderPath.Location = new System.Drawing.Point(77, 24);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.ReadOnly = true;
+            this.txtFolderPath.Size = new System.Drawing.Size(594, 20);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件夹：";
+            // 
+            // groupBoxFiles
+            // 
+            this.groupBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxFiles.Controls.Add(this.btnRefresh);
+            this.groupBoxFiles.Controls.Add(this.btnDeselectAll);
+            this.groupBoxFiles.Controls.Add(this.btnSelectAll);
+            this.groupBoxFiles.Controls.Add(this.lstFiles);
+            this.groupBoxFiles.Location = new System.Drawing.Point(12, 78);
+            this.groupBoxFiles.Name = "groupBoxFiles";
+            this.groupBoxFiles.Size = new System.Drawing.Size(760, 284);
+            this.groupBoxFiles.TabIndex = 1;
+            this.groupBoxFiles.TabStop = false;
+            this.groupBoxFiles.Text = "文件列表";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRefresh.Location = new System.Drawing.Point(174, 255);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "刷新列表";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnDeselectAll
+            // 
+            this.btnDeselectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDeselectAll.Location = new System.Drawing.Point(93, 255);
+            this.btnDeselectAll.Name = "btnDeselectAll";
+            this.btnDeselectAll.Size = new System.Drawing.Size(75, 23);
+            this.btnDeselectAll.TabIndex = 2;
+            this.btnDeselectAll.Text = "取消全选";
+            this.btnDeselectAll.UseVisualStyleBackColor = true;
+            this.btnDeselectAll.Click += new System.EventHandler(this.btnDeselectAll_Click);
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSelectAll.Location = new System.Drawing.Point(12, 255);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectAll.TabIndex = 1;
+            this.btnSelectAll.Text = "全选";
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(12, 19);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstFiles.Size = new System.Drawing.Size(740, 225);
+            this.lstFiles.TabIndex = 0;
+            // 
+            // groupBoxNewExtension
+            // 
+            this.groupBoxNewExtension.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxNewExtension.Controls.Add(this.label3);
+            this.groupBoxNewExtension.Controls.Add(this.txtNewExtension);
+                        this.groupBoxNewExtension.Controls.Add(this.label2);
+            this.groupBoxNewExtension.Location = new System.Drawing.Point(12, 368);
+            this.groupBoxNewExtension.Name = "groupBoxNewExtension";
+            this.groupBoxNewExtension.Size = new System.Drawing.Size(760, 60);
+            this.groupBoxNewExtension.TabIndex = 2;
+            this.groupBoxNewExtension.TabStop = false;
+            this.groupBoxNewExtension.Text = "扩展名更改";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "例如: jpg, txt, pdf (不需要输入点号)";
+            // 
+            // txtNewExtension
+            // 
+            this.txtNewExtension.Location = new System.Drawing.Point(77, 24);
+            this.txtNewExtension.Name = "txtNewExtension";
+            this.txtNewExtension.Size = new System.Drawing.Size(91, 20);
+            this.txtNewExtension.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "新扩展名:";
+            // 
+            // btnApply
+            // 
+            this.btnApply.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnApply.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApply.Location = new System.Drawing.Point(354, 434);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 3;
+            this.btnApply.Text = "应用更改";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 467);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(55, 17);
+            this.lblStatus.Text = "准备就绪";
+            // 
+            // FileExtensionChangerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 489);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.groupBoxNewExtension);
+            this.Controls.Add(this.groupBoxFiles);
+            this.Controls.Add(this.groupBoxFolder);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "FileExtensionChangerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文件扩展名批量更改器";
+            this.groupBoxFolder.ResumeLayout(false);
+            this.groupBoxFolder.PerformLayout();
+            this.groupBoxFiles.ResumeLayout(false);
+            this.groupBoxNewExtension.ResumeLayout(false);
+            this.groupBoxNewExtension.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxFolder;
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBoxFiles;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnDeselectAll;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.GroupBox groupBoxNewExtension;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNewExtension;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+    }
+}
---
+++ FileExtensionChanger/FileExtensionChanger.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileExtensionChanger
+{
+    public partial class FileExtensionChangerForm : Form
+    {
+        private List<string> selectedFiles = new List<string>();
+        
+        public FileExtensionChangerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowser = new FolderBrowserDialog())
+            {
+                folderBrowser.Description = "选择包含要修改的文件的文件夹";
+                
+                if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    txtFolderPath.Text = folderBrowser.SelectedPath;
+                    LoadFilesFromFolder(folderBrowser.SelectedPath);
+                }
+            }
+        }
+
+        private void LoadFilesFromFolder(string folderPath)
+        {
+            try
+            {
+                lstFiles.Items.Clear();
+                selectedFiles.Clear();
+                
+                string[] files = Directory.GetFiles(folderPath);
+                foreach (string file in files)
+                {
+                    lstFiles.Items.Add(Path.GetFileName(file));
+                }
+                
+                lblStatus.Text = $"找到 {files.Length} 个文件";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"加载文件夹内容时出错: {ex.Message}", "错误", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSelectAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lstFiles.Items.Count; i++)
+            {
+                lstFiles.SetSelected(i, true);
+            }
+        }
+
+        private void btnDeselectAll_Click(object sender, EventArgs e)
+        {
+            lstFiles.ClearSelected();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtFolderPath.Text) && Directory.Exists(txtFolderPath.Text))
+            {
+                LoadFilesFromFolder(txtFolderPath.Text);
+            }
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            if (lstFiles.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("请至少选择一个文件", "提示", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string newExtension = txtNewExtension.Text.Trim();
+            if (string.IsNullOrEmpty(newExtension))
+            {
+                MessageBox.Show("请输入新的扩展名", "提示", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            // 确保扩展名格式正确，不包含点号
+            newExtension = newExtension.TrimStart('.');
+
+            string folderPath = txtFolderPath.Text;
+            int successCount = 0;
+            List<string> failedFiles = new List<string>();
+
+            foreach (var item in lstFiles.SelectedItems)
+            {
+                string oldFilename = item.ToString();
+                string oldFilePath = Path.Combine(folderPath, oldFilename);
+                
+                // 分离文件名和扩展名
+                string baseName = Path.GetFileNameWithoutExtension(oldFilename);
+                string newFilename = $"{baseName}.{newExtension}";
+                string newFilePath = Path.Combine(folderPath, newFilename);
+                
+                try
+                {
+                    // 如果新文件已存在，先询问是否覆盖
+                    if (File.Exists(newFilePath) && oldFilename != newFilename)
+                    {
+                        DialogResult result = MessageBox.Show(
+                            $"文件 '{newFilename}' 已存在，是否覆盖?", 
+                            "文件已存在",
+                            MessageBoxButtons.YesNo, 
+                            MessageBoxIcon.Question);
+                            
+                        if (result == DialogResult.No)
+                        {
+                            failedFiles.Add($"{oldFilename} (用户取消)");
+                            continue;
+                        }
+                        
+                        // 如果用户同意覆盖，先删除目标文件
+                        File.Delete(newFilePath);
+                    }
+                    
+                    // 重命名文件
+                    File.Move(oldFilePath, newFilePath);
+                    successCount++;
+                }
+                catch (Exception ex)
+                {
+                    failedFiles.Add($"{oldFilename} ({ex.Message})");
+                }
+            }
+
+            // 刷新文件列表
+            LoadFilesFromFolder(folderPath);
+            
+            // 显示结果
+            string resultMessage = $"成功更改了 {successCount} 个文件的扩展名";
+            if (failedFiles.Count > 0)
+            {
+                resultMessage += $"\n\n以下文件处理失败:\n{string.Join("\n", failedFiles)}";
+            }
+            
+            MessageBox.Show(resultMessage, "处理结果", 
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            lblStatus.Text = $"成功更改了 {successCount} 个文件的扩展名";
+        }
+    }
+}
